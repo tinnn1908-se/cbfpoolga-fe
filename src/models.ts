8635e386e645e790d042add1e6572d029844e6b2
tinnn1908-se <tinnn1908.se@gmail.com>
2022-08-24 11:28:24 -0500

    changes

--- src/models.ts
+++ src/models.ts
@@ -12,3 +12,34 @@ export interface Game {
         score: string
     }
 }
+
+export interface Picking {
+    id: string,
+    entry: string,
+    user_id: string,
+    tiebreak: number,
+    isChanged : boolean
+}
+
+export interface Pickingdetails {
+    awayteam: string,
+    awayscore: string,
+    awaynumber: number,
+    hometeam: string,
+    homescore: string,
+    homenumber: number,
+    selected_team_number: number,
+    isLastgame: boolean,
+    pickingId: string,
+    isChanged : boolean
+}
+
+export interface User {
+    id: string,
+    username: string,
+    password: string,
+    email: string,
+    created_date: string,
+    is_activated: boolean,
+    is_deleted: boolean
+}
